bccc2890402bfa8983b99ed0c02a817ff1af3fa1
gabriel <gabrielpr9520@gmail.com>
2025-01-23 13:01:29 +0000

    cambios jueves mañana

--- app/Livewire/AbsenceComponent.php
+++ app/Livewire/AbsenceComponent.php
@@ -6,8 +6,26 @@ use Livewire\Component;
 
 class AbsenceComponent extends Component
 {
+    public $absences=[];
+    public $user;
+    public $absence;
+    public $description;
+    public $hour;
+    public $turn;
+    public $user_id;
     public function render()
     {
         return view('livewire.absence-component');
     }
+public function createAbsence(){}
+public function openAbsenceForm(){}
+public function closeAbsenceForm(){}
+public function deleteAbsence(){}
+public function getAbsences(){}
 }
+/*
+
+ 'description',
+        'hour',
+        'turn',
+        'user_id',*/
